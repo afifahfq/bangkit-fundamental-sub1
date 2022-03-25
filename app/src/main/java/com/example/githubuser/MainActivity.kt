e40a4f560e9c0b9213b897a5e1d40b6727e1f5ec
afifahfq <afifahfq@gmail.com>
2022-03-25 21:31:10 +0700

    finish search feature

--- app/src/main/java/com/example/githubuser/MainActivity.kt
+++ app/src/main/java/com/example/githubuser/MainActivity.kt
@@ -35,34 +35,14 @@ class MainActivity : AppCompatActivity() {
         rvUsers = findViewById(R.id.rv_users)
         rvUsers.setHasFixedSize(true)
 
-        findUsers()
-        //Log.i("cekpoin", "HA")
+        findUsers(USER_USERNAME)
     }
 
-//    private val listUsers: ArrayList<User>
-//        get() {
-//            val dataUsername = resources.getStringArray(R.array.username)
-//            val dataName = resources.getStringArray(R.array.name)
-//            val dataLocation = resources.getStringArray(R.array.location)
-//            val dataRepository = resources.getStringArray(R.array.repository)
-//            val dataCompany = resources.getStringArray(R.array.company)
-//            val dataFollowers = resources.getStringArray(R.array.followers)
-//            val dataFollowing = resources.getStringArray(R.array.following)
-//            val dataAvatar = resources.getStringArray(R.array.avatar)
-//            //val dataAvatar = resources.obtainTypedArray(R.array.avatar)
-//            val listUser = ArrayList<User>()
-//            for (i in dataName.indices) {
-//                val user = User(dataUsername[i], dataName[i], dataLocation[i], dataRepository[i], dataCompany[i], dataFollowers[i],  dataFollowing[i], dataAvatar[i])
-//                //val user = User(dataUsername[i], dataName[i], dataLocation[i], dataRepository[i], dataCompany[i], dataFollowers[i],  dataFollowing[i], dataAvatar.getResourceId(i, -1))
-//                listUser.add(user)
-//            }
-//        Log.i("cekListUser", listUser.toString())
-//        return listUser
-//    }
+    private fun findUsers(username: String) {
+        list.clear()
 
-    private fun findUsers() {
         showLoading(true)
-        val client = ApiConfig.getApiService().searchUser(USER_USERNAME)
+        val client = ApiConfig.getApiService().searchUser(username)
         client.enqueue(object : Callback<ApiResponse> {
             override fun onResponse(
                 call: Call<ApiResponse>,
@@ -95,42 +75,6 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-//    private fun findUser() {
-//        showLoading(true)
-//        val client = ApiConfig.getApiService().getUser(USER_USERNAME)
-//        client.enqueue(object : Callback<ApiResponse> {
-//            override fun onResponse(
-//                call: Call<ApiResponse>,
-//                response: Response<ApiResponse>
-//            ) {
-//                showLoading(false)
-//                val responseBody = response.body()
-//                if (responseBody != null) {
-//                    for (user in responseBody.ApiResponse!!) {
-//                        var curr = User(
-//                            user?.login,
-//                            user?.htmlUrl,
-//                            user?.htmlUrl,
-//                            user?.reposUrl,
-//                            user?.htmlUrl,
-//                            user?.followersUrl,
-//                            user?.followingUrl,
-//                            user?.avatarUrl )
-//
-//                        list.add(curr)
-//                    }
-//
-//                    //list.addAll(listUsers)
-//                    //showRecyclerList()
-//                }
-//            }
-//            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
-//                showLoading(false)
-//                Log.e(TAG, "onFailure: ${t.message}")
-//            }
-//        })
-//    }
-
     private fun showRecyclerList() {
         if (applicationContext.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             rvUsers.layoutManager = GridLayoutManager(this, 2)
@@ -159,38 +103,39 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        val inflater = menuInflater
-//        inflater.inflate(R.menu.option_menu, menu)
-//
-//        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
-//        val searchView = menu.findItem(R.id.search).actionView as SearchView
-//
-//        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
-//        searchView.queryHint = resources.getString(R.string.search_hint)
-//        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-//            override fun onQueryTextSubmit(query: String): Boolean {
-//                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
-//                searchView.clearFocus()
-//                return true
-//            }
-//            override fun onQueryTextChange(newText: String): Boolean {
-//                return false
-//            }
-//        })
-//
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when (item.itemId) {
-//            R.id.menu1 -> {
-//                startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
-//                return true
-//            }
-//            else -> return true
-//        }
-//    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                //Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                findUsers(query)
+                searchView.clearFocus()
+                return true
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu1 -> {
+                startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+                return true
+            }
+            else -> return true
+        }
+    }
 
     companion object {
         private const val TAG = "MainActivity"
